6915e9980be07058277e30b8b8078aa4745145c7
apple <apple@appurunoiMac.local>
2020-08-12 13:04:45 +0900

    解像度変更できるようにした

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,12 @@
   <div>
     <h1>Vuejs Hlsjs</h1>
 
-    <video
-      ref="video"
-      id="video"
-      controls
-      webkit-playsinline
-      muted
-      autoplay
-    ></video>
+    <video ref="video" id="video" controls webkit-playsinline muted autoplay height="1500"></video>
     <button @click="PlayVide()">動画を再生する</button>
-    <button @click="changeResolution(4)">level4を再生</button>
-    <button @click="changeResolution(0)">level1を再生</button>
+    <button @click="changeResolution(3)">1080pを再生</button>
+    <button @click="changeResolution(0)">360pを再生</button>
 
-    <img src="../public/logo.png" alt="image" class="img" />
+    <!-- <img src="../public/logo.png" alt="image" class="img" /> -->
     <!-- <img src="./assets/Icon_3d.png" alt="" /> -->
   </div>
 </template>
@@ -25,23 +18,22 @@ import Hls from "hls.js";
 export default {
   data() {
     return {
+      // hlsjsを機能させるための初期化処理を格納した変数
       hls: new Hls()
     };
   },
   methods: {
     PlayVide() {
-      var video = document.getElementById("video");
+      const video = document.getElementById("video");
       if (Hls.isSupported()) {
-        var hls = new Hls();
+        this.hls = new Hls();
         // hls.currentLevel = n;
-        // hls.loadSource(
-        //   "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
-        // );
-        hls.loadSource("./video/playlist.m3u8");
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
+        this.hls.loadSource("./video/playlist.m3u8");
+        this.hls.attachMedia(video);
+        this.hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
           console.log(data.levels);
-          hls.currentLevel();
+          console.log(event);
+          this.hls.currentLevel();
           // hls.autoLevelEnabled = false;
           // hls.currentLevel = 3;
           video.play();
@@ -49,15 +41,17 @@ export default {
       }
     },
     changeResolution(n) {
+      console.log(n);
+      // var video = document.getElementById("video");
       //ボタンにある引数を取ってきてそれをhls.currentLevelに代入することで
       //解像度が変わる
       //引数は取れてきてるけど、hls.currentLevelに入れれてない
-      var hls = new Hls();
-      hls.currentLevel = n;
+      // var hls = new Hls();
+      this.hls.currentLevel = n;
 
-      console.log(n); //引数の値表示、1 or 4
+      console.log(n); //引数の値表示、1 or
       setTimeout(() => {
-        console.log(hls.currentLevel);
+        console.log(this.hls.currentLevel);
       }, 1000);
     }
   }
